5d84f9c0482d50fe3133e8bd5fd1ecc0a6a825ea
Robert Birnie <rbirnie@gmail.com>
2013-11-22 09:15:29 -0800

    strings should be symbols to match documentation

--- app/overrides/add_column_content.rb
+++ app/overrides/add_column_content.rb
@@ -1,5 +1,5 @@
 if SETTINGS[:column_view]
-  SETTINGS[:column_view].reject { |k,v| v[:view] && v[:view] != 'hosts_list' }.keys.sort.map do |k|
+  SETTINGS[:column_view].reject { |k,v| v[:view] && v[:view] != :hosts_list }.keys.sort.map do |k|
     after = SETTINGS[:column_view][k.to_sym][:after]
     Deface::Override.new(
       :virtual_path => "hosts/_list",
