9ad8de53e89bd625605fa4df170828abf4b7035e
Tomer Brisker <tbrisker@gmail.com>
2015-10-14 14:43:52 +0300

    Handle column widths and overflow

--- app/overrides/add_column_title.rb
+++ app/overrides/add_column_title.rb
@@ -1,11 +1,12 @@
 if SETTINGS[:column_view]
   SETTINGS[:column_view].reject { |k,v| v[:view] && v[:view] != :hosts_list }.keys.sort.map do |k|
     after = SETTINGS[:column_view][k.to_sym][:after]
+    width = SETTINGS[:column_view][k.to_sym][:width] || "10%"
     Deface::Override.new(
       :virtual_path => "hosts/_list",
       :name => "title_#{k}",
       :insert_after => "th:contains('#{after}')",
-      :text => "\n    <th class=\"hidden-tablet hidden-phone\"><%= fcv_title '#{k}' %></th>"
+      :text => "\n    <th class=\"hidden-tablet hidden-phone\" width=\"#{width}%\"><%= fcv_title '#{k}' %></th>"
     )
   end
 end
